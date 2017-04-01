c766105f610d230940a71195b2b84330eb71acbb
Andersonmendes2016 <lge500meu@gmail.com>
2017-04-01 11:36:35 -0300

    DU-ify

--- lineage.mk
+++ lineage.mk
@@ -1,8 +1,12 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_condor
+PRODUCT_NAME := du_condor
 PRODUCT_RELEASE_NAME := MOTO E
