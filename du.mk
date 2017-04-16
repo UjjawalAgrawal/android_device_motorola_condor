e603b81bde949ed1d5e6191c800f1a246f372492
Anderson Mendes <lge500meu@gmail.com>
2017-04-16 10:07:37 -0300

    aex ify

--- du.mk
+++ du.mk
@@ -1,12 +1,16 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
-# Inherit some common DU stuff.
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Bootanimation
 TARGET_SCREEN_HEIGHT := 960
 TARGET_SCREEN_WIDTH := 540
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := du_condor
+PRODUCT_NAME := aosp_condor
 PRODUCT_RELEASE_NAME := MOTO E
+PRODUCT_MODEL := XT1025
+
+#Build Type
+EXTENDED_BUILD_TYPE=OFFICIAL
