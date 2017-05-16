e13608a3b25af89c20d492be78d4a97754de3822
Anderson Mendes <lge500meu@gmail.com>
2017-05-16 16:36:51 -0300

    PN ify

--- aosp_condor.mk
+++ aosp_condor.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
-# Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Bootanimation
 TARGET_SCREEN_HEIGHT := 960
@@ -10,7 +10,3 @@ TARGET_SCREEN_WIDTH := 540
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosp_condor
 PRODUCT_RELEASE_NAME := MOTO E
-PRODUCT_MODEL := XT1025
-
-#Build Type
-EXTENDED_BUILD_TYPE=OFFICIAL
